40453cf90ab28fccee7ca08814e556be2c2dcc94
Lisa <yhsu16@gmail.com>
2016-10-07 17:36:42 -0400

    added lines to save the sample data

--- scala-spark/src/main/scala/com/devinline/spark/preprocessing.scala
+++ scala-spark/src/main/scala/com/devinline/spark/preprocessing.scala
@@ -90,7 +90,7 @@ object preprocessing {
     val sc = new SparkContext(conf)
 
     //Read some example file to a data RDD
-    val data = sc.textFile("data/state13.csv")
+    val data = sc.textFile("data/state13_first_1k.csv")
     val data_header = data.first()
     val summary_data = sc.textFile("data/summary_first_1k.csv")
     val summary_header = summary_data.first()
@@ -116,20 +116,23 @@ object preprocessing {
     
     // data of donors that have activities after 2000
     var data_after_2000 = data_splitted.filter( row => donor_after_2000.contains(row(0)))
-
+    
     //Count times of donations by arc_id
     val donate_cnt = data_after_2000.map { line => (line(0), 1) }.reduceByKey(_ + _) //Sum all of the value with same key
     //Save arc_id into separate lists of repeat donor and one-time donor
-    //TODO: Remove the comments for file saving
-    val repeat_donor = donate_cnt.filter(v => v._2 > 1 ).map(v => v._1)//.saveAsTextFile("data/temp1")
-    val onetime_donor = donate_cnt.filter(v => v._2 == 1 ).map(v => v._1)//.saveAsTextFile("data/temp2")
+    val repeat_donor = donate_cnt.filter(v => v._2 > 1 ).map(v => v._1)
+    repeat_donor.saveAsTextFile("data/repeat_donor_list")
+    val onetime_donor = donate_cnt.filter(v => v._2 == 1 ).map(v => v._1)
+    onetime_donor.saveAsTextFile("data/onetime_donor_list")
     
     val repeat_donor_sample = repeat_donor.sample(false, 0.01, 100)
+    val repeat_donor_array = repeat_donor_sample.collect()
     val onetime_donor_sample = onetime_donor.sample(false, 0.01, 100)
-    val sample_donors = repeat_donor_sample.collect() ++ onetime_donor_sample.collect()
-    var sample_data = data_after_2000.filter { row => sample_donors contains row(0) }
+    val onetime_donor_array = onetime_donor_sample.collect()
+    val sample_donors = repeat_donor_array ++ onetime_donor_array
+    var sample_data = data_after_2000.filter { row => sample_donors.contains(row(0)) }.map { x => is_repeat_donor(x, onetime_donor_array, repeat_donor_array) }
     
-    sample_data.saveAsTextFile("data/sample_data")
+    sample_data.map { x => x.mkString(",") }.saveAsTextFile("data/sample_data")
 
     //Stop the Spark context
     sc.stop
